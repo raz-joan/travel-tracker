48b9441d52a63f2c623720ed914feb3cc5aa8bf3
Joan Rasmussen <raz.joan@gmail.com>
2021-11-10 19:44:02 -0700

    Update image imports in the scripts.js

--- src/scripts.js
+++ src/scripts.js
@@ -5,7 +5,10 @@
 import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import './images/home-icon.png';
+import './images/ticket-icon.png';
+import './images/logout-icon.png';
+import './images/traveling-icon.png';
 
 
-console.log('This is the JavaScript entry file - your code begins here.');
+// console.log('This is the JavaScript entry file - your code begins here.');
