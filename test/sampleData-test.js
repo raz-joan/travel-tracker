ff7c182dbbf81da9d2535c0f85f2cee0a04cc072
Joan Rasmussen <raz.joan@gmail.com>
2021-11-12 14:31:38 -0700

    Reduce the sampleData and write tests for Trip class

--- test/sampleData-test.js
+++ test/sampleData-test.js
@@ -28,22 +28,6 @@ const destinationTestData = [
     "estimatedFlightCostPerPerson": 950,
     "image": "https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
     "alt": "opera house and city buildings on the water with boats"
-    },
-    {
-    "id": 4,
-    "destination": "Cartagena, Colombia",
-    "estimatedLodgingCostPerDay": 65,
-    "estimatedFlightCostPerPerson": 350,
-    "image": "https://images.unsplash.com/photo-1558029697-a7ed1a4b94c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
-    "alt": "boats at a dock during the day time"
-    },
-    {
-    "id": 5,
-    "destination": "Madrid, Spain",
-    "estimatedLodgingCostPerDay": 150,
-    "estimatedFlightCostPerPerson": 650,
-    "image": "https://images.unsplash.com/photo-1543785734-4b6e564642f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-    "alt": "city with clear skys and a road in the day time"
     }
 ];
 
@@ -51,7 +35,7 @@ const tripTestData = [
   {
     "id": 1,
     "userID": 1,
-    "destinationID": 5,
+    "destinationID": 1,
     "travelers": 3,
     "date": "2019/10/22",
     "duration": 7,
@@ -62,7 +46,7 @@ const tripTestData = [
     "id": 2,
     "userID": 1,
     "destinationID": 3,
-    "travelers": 3,
+    "travelers": 2,
     "date": "2020/02/03",
     "duration": 14,
     "status": "approved",
@@ -73,29 +57,9 @@ const tripTestData = [
     "userID": 1,
     "destinationID": 2,
     "travelers": 1,
-    "date": "2020/07/17",
+    "date": "2022/07/17",
     "duration": 5,
-    "status": "approved",
-    "suggestedActivities": []
-  },
-  {
-    "id": 4,
-    "userID": 1,
-    "destinationID": 1,
-    "travelers": 2,
-    "date": "2021/02/20",
-    "duration": 18,
-    "status": "approved",
-    "suggestedActivities": []
-  },
-  {
-    "id": 5,
-    "userID": 1,
-    "destinationID": 4,
-    "travelers": 1,
-    "date": "2022/09/16",
-    "duration": 8,
-    "status": "approved",
+    "status": "pending",
     "suggestedActivities": []
   }
 ];
